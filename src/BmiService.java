ad18a77ea037fd526a2905e8e7b62b413b754412
Aleant <alalant50@gmail.com>
2022-06-23 23:42:45 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(int m, double h){
-        return m / Math.pow(h/100,2);
+    public double calculate(int mass, double height) {
+        return mass / Math.pow(height / 100, 2);
     }
 }
